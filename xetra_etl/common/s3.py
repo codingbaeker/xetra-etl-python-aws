e34848d56c01008dd0170e5e65ef17fea5cb3dee
HimmelsbachJannik <Jannik.Himmelsbach@enroutesystems.com>
2022-05-23 16:48:12 -0500

    added fuctions write_df_to_s3 and read_csv_as_df

--- xetra_etl/common/s3.py
+++ xetra_etl/common/s3.py
@@ -1,8 +1,10 @@
 """Connector and methods accessing S3"""
+from io import BytesIO, StringIO
 import os
 import logging
 
 import boto3
+import pandas as pd
 
 class S3BucketConnector():
     """
@@ -15,7 +17,7 @@ class S3BucketConnector():
         :param access_key: access key for accessing S3
         :param secret_key: secret key for accessing S3
         :param endpoint_url: endpoint url to S3
-        : param bucket: S3 bucket name
+        :param bucket: S3 bucket name
         """
         self._logger = logging.getLogger(__name__)
         self.endpoint_url = endpoint_url
@@ -38,8 +40,33 @@ class S3BucketConnector():
         files = [obj.key for obj in self._bucket.objects.filter(Prefix=prefix)]
         return files
 
-    def read_csv_as_df(self):
-        pass
+    def read_csv_as_df(self, bucket: str, key: str, decoding: str = 'utf-8', sep: str = ','):
+        """
+        read csv file from bucket and put its data in a dataframe
 
-    def write_df_to_s3(self):
-        pass
+        :param bucket: S3 bucket name
+        :param key:  key of csv file in S3 bucket
+        :param decoding: encoding method
+        :param sep: delimiter of the file
+
+        returns:
+            dataframe: dataframe with the data of the csv file
+        """
+        csv_obj = bucket.Object(key=key).get().get('Body').read().decode(decoding)
+        data = StringIO(csv_obj)
+        dataframe = pd.read_csv(data, delimiter=sep)
+        return dataframe
+
+    def write_df_to_s3(self, bucket: str, dataframe: str, key: str):
+        """
+        writes dataframe to S3 Bucket
+
+        :param bucket: S3 bucket name
+        :param dataframe: Pandas dataframe
+        :param key: key to save file in S3 Bucket
+        """
+        out_buffer = BytesIO()
+        dataframe.to_parquet(out_buffer, index=False)
+        bucket.put_object(Body=out_buffer.getvalue(), Key=key)
+        return True
+        
